0c332b88a2e4cc35b0644b12fb86629aa9a16717
JulianoF <juliano1@live.ca>
2023-09-29 19:04:08 -0400

    Working File Reading + Temp Test Code

--- src/main/java/Main/CashRegister.java
+++ src/main/java/Main/CashRegister.java
@@ -7,11 +7,28 @@ import java.util.Scanner;
 
 public class CashRegister {
     private final File productFile;
+    Scanner lineReader;
+    //private String[][] db = new String[4][4];
     
     public CashRegister() throws FileNotFoundException{
-        this.productFile = new File("ProductList.txt");
-        Scanner lineReader = new Scanner(this.productFile);
+        this.productFile = new File("src/main/java/Main/ProductList.txt");
+        lineReader = new Scanner(this.productFile);
+
     }
     
+    public void getItem(int iD){
+        while(lineReader.hasNext()){
+            if(lineReader.hasNextInt()){
+                int current = lineReader.nextInt();
+                String name = lineReader.next();
+                Float price = lineReader.nextFloat();
+                System.out.println(current);
+                System.out.println(name);
+                System.out.println(price);
+            }else{
+                lineReader.next();
+            }
+        }
+    }
     
 }
--- src/main/java/Main/MainTestClass.java
+++ src/main/java/Main/MainTestClass.java
@@ -1,11 +1,20 @@
 package Main;
 
+import java.io.FileNotFoundException;
+
 public class MainTestClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Keyboard testKeyboard = new Keyboard();
         Display testDisplay = new Display();
         
+        //TEST CODE
+        System.out.println(System.getProperty("user.dir"));
+        CashRegister test = new CashRegister();
+        test.getItem(1);
+        test.getItem(2);
+        test.getItem(3);
+        test.getItem(4);
         
     }
 }
