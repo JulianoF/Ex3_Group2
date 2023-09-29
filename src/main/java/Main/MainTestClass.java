26f727ba7bb19e2a7569efd45adbdfc87de72b8a
JulianoF <juliano1@live.ca>
2023-09-29 18:41:56 -0400

    First init

---
+++ src/main/java/Main/MainTestClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author juliano
+ */
+public class MainTestClass {
+
+    public static void main(String[] args) {
+        
+    }
+}
