74eed90952daa8cf8efff511aeac1b48f61cbde7
DemiwizAces <erdmriosnadeau@gmail.com>
2023-09-30 13:59:11 -0400

    Added methods to Display Class and instantiated Display object in Cashregister

--- src/main/java/Main/CashRegister.java
+++ src/main/java/Main/CashRegister.java
@@ -8,32 +8,36 @@ import java.util.Scanner;
 public class CashRegister{
     private final File productFile;
     private Scanner lineReader;
+    public Display primaryDisplay;
     
     public CashRegister() throws FileNotFoundException{
         this.productFile = new File("src/main/java/Main/ProductList.txt");
         this.lineReader = null;
-
+        primaryDisplay = new Display();
     }
     
     public void getItem(int iD) throws FileNotFoundException{
         this.lineReader = new Scanner(this.productFile);
+        String productDetails = "";
+        boolean hasProduct = false;
         while(lineReader.hasNext()){
             if(lineReader.hasNextInt()){
                 int current = lineReader.nextInt();
                 if(current == iD){
                 String name = lineReader.next();
                 float price = lineReader.nextFloat();
-                
+                hasProduct = true; 
                 ////TEST CODE////
-                System.out.println(current);
-                System.out.println(name);
-                System.out.println(price);}
+                productDetails = "Product ID:" + current + "\nProduct: " + 
+                name + "\nPrice $" + price+ "\n";}                
                 ////////////////
                 
             }else{
                 lineReader.next();
             }
         }
+        primaryDisplay.notifyDisplay(hasProduct);
+        primaryDisplay.updateProduct(productDetails);
     }
     
 }
--- src/main/java/Main/Display.java
+++ src/main/java/Main/Display.java
@@ -2,4 +2,18 @@ package Main;
 
 public class Display {
     public Display(){}
+    
+    public void notifyDisplay(boolean productDetect) {
+    	if(productDetect) {
+    		System.out.println("Product Detected");
+    	}
+    	else {
+    		System.out.println("No Matching Product found, please try again");
+    	}
+    }
+    
+    public void updateProduct(String productDetails) {
+    	if(!productDetails.isEmpty())
+    	System.out.println(productDetails);
+    }
 }
